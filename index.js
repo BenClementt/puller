8d83b69f28376539ae37f12c16647d508de5ec41
Callum OKane <35779365+callumok2004@users.noreply.github.com>
2021-06-14 21:13:44 +0100

    idk
    i like cleaning code ig

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-const { exec } = require("child_process");
-const cron = require('node-cron')
-const chalk = require('chalk')
-
-var gitFolders = ['/var/www/html/']
+const { exec } = require("child_process"),
+      cron = require('node-cron',
+      chalk = require('chalk'),
+      gitFolders = ['/var/www/html/'];
 
 cron.schedule('* * * * *', () => {
     var i;
-    for(i=0; i < gitFolders.length; i++){
+    for(i=0; i < gitFolders.length; i++) {
         exec(`cd ${gitFolders[i]} && git pull`, (error, stdout, stderr) => {
-            if(error){
-                console.log(chalk.red("[ERROR]" + chalk.white(" ") + chalk.white(error)))
-                return;
-            }
-            if(stderr){
-                console.log(chalk.green("[PULLED]" + chalk.white(" ") + chalk.white(stderr)))
-                return;
-            }
+            if (error) return console.log(chalk.red("[ERROR]" + chalk.white(" ") + chalk.white(error)));
+            if (stderr) return console.log(chalk.green("[PULLED]" + chalk.white(" ") + chalk.white(stderr)));
 
-            console.log(chalk.blue("[INFO]" + chalk.white(" ") + chalk.white(stdout)))
+            console.log(chalk.blue("[INFO]" + chalk.white(" ") + chalk.white(stdout)));
         })
     }
 })
